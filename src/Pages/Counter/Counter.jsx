bdebc22d5fae4b33202218e9ebc188a33270b531
SShipon <nxshipon@gmail.com>
2022-08-18 12:15:30 +0600

    updated code
    Signed-off-by: SShipon <nxshipon@gmail.com>

--- src/Pages/Counter/Counter.jsx
+++ src/Pages/Counter/Counter.jsx
@@ -23,7 +23,7 @@ function Counter({ count, increment, decrement }) {
         </div>
         <div class="d-flex my-2">
           <button href="#" class="btn btn-primary mx-2 ms-auto" id="addCounter">
-            +Add Counter count
+            +Add Counter count this is hello
           </button>
           <button href="#" class="btn btn-danger" id="rest">
             Rest helllo
